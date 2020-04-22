646e7257905b503f0ef36080e4c23db249cdd0bb
kwnhyk <kwnhyk@naver.com>
2020-04-23 05:25:09 +0900

    Updates

---
+++ precache-manifest.f1741056ff744f49f8c256f93ce565d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27b2b5a7a7081276751f0c2abdcccaf1",
+    "url": "/kwnhyk_movie_app/index.html"
+  },
+  {
+    "revision": "caed16e016975177f740",
+    "url": "/kwnhyk_movie_app/static/css/main.807c7579.chunk.css"
+  },
+  {
+    "revision": "141b5781bd1599b81812",
+    "url": "/kwnhyk_movie_app/static/js/2.b22c16a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kwnhyk_movie_app/static/js/2.b22c16a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caed16e016975177f740",
+    "url": "/kwnhyk_movie_app/static/js/main.93e18ce2.chunk.js"
+  },
+  {
+    "revision": "33bd123379c9bbf42b8e",
+    "url": "/kwnhyk_movie_app/static/js/runtime-main.a0d31b6e.js"
+  }
+]);
